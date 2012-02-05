0eea81ef9a57d3d0a6eece23f87c05050b4c141d
Adrian <akuri88@gmail.com>
2012-02-05 01:53:03 -0600

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+gem 'databasedotcom', :git => 'git://github.com/heroku/databasedotcom.git'
+gem 'databasedotcom-rails'
+gem 'heroku'
+gem 'jquery-rails'
+
+group :development do
+	gem 'rspec-rails'
+end
+
+group :test do
+	gem 'rspec'
+	gem 'webrat'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jueguebalon::Application.config.secret_token = '2bd5bafefec401a93947612a4a7ae9178e149f6f15f5df493b0ea285b3f9b7ddb7145617531d8db19242299209f28055583b24d8a4a7e08b95bf10803959b999'
